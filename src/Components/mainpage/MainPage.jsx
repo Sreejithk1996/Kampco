47adbbde8013532678f5e042268082db2f322716
Sreejithk1996 <sreekumar1996@gmail.com>
2024-03-12 17:25:49 +0530

    updated

--- src/Components/mainpage/MainPage.jsx
+++ src/Components/mainpage/MainPage.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Dashboard from '../dashboard/Dashboard'
 import Navigation from '../navigation/Navigation'
+import search from '../../assets/images/search-icon.svg'
+import bell from '../../assets/images/bell-icon.svg'
+import profile from '../../assets/images/profile-pic.svg'
 
 function MainPage() {
   return (
     <div className='dashboard flex gap-[50px]'>
       <Navigation />
-      <div className='dashboard-main'>
-        <div className='dashboard-top'>
-          <input className='search w-[545px] h-[50px] rounded-[10px] border-[1px] border-[#D4D4D4] bg-[#F9FAFB] px-[15px] py-[9px]' type='text' placeholder='Search' ></input>
-          <div className='profile'>
-            <div className='notification'>
-
+      <div className='dashboard-main w-[100%]'>
+        <div className='dashboard-top  flex items-center justify-between gap-[30px]'>
+          <input className='search w-[545px] h-[50px] rounded-[10px] border-[1px] border-[#D4D4D4] bg-[#F9FAFB] px-[15px] py-[9px]' type='text' placeholder=' Search' ></input>
+          <div className='profile flex gap-[20px]'>
+            <div className='notification w-[50px] h-[50px] border-[1px] border-[#D4D4D4] flex justify-center items-center rounded-[10px]'>
+              <img src={bell} alt="bell-icon" />
             </div>
-            <div className='login'>
-
+            <div className='login flex px-[15px] py-[5px] font-[700] border-[1px] border-[#D4D4D4] rounded-[10px] items-center gap-[5px]'>
+              <img src={profile} alt="profilepic" />
+              <p>Super Admin</p>
             </div>
           </div>
         </div>
