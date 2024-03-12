74c9c79a49020661416d82acab34cda4067dc0ad
Sreejithk1996 <sreekumar1996@gmail.com>
2024-03-12 18:11:25 +0530

    updated

--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -7,8 +7,8 @@ function Dashboard() {
         <p className='text-[26px] font-[700]'>Overview</p>
         <p className='text-[18px] text-[#666]'>Hellow Admin!!!</p>
       </div>
-      <div className='month p-[20px] border-[1px] rounded-[10px]'>
-
+      <div className='month  p-[20px] border-[1px] rounded-[10px] h-[50px] w-[150px] flex justify-center items-center'>
+        <p className=''>This Month</p>
       </div>
     </div>
   )
