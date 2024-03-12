a6cedd90132922243ab584fc013a405d5051ba12
Sreejithk1996 <sreekumar1996@gmail.com>
2024-03-12 18:04:30 +0530

    dashboard content added

--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
-      
+    <div className='overview flex w-[100%] justify-between'>
+      <div className='overview-sub'>
+        <p className='text-[26px] font-[700]'>Overview</p>
+        <p className='text-[18px] text-[#666]'>Hellow Admin!!!</p>
+      </div>
+      <div className='month p-[20px] border-[1px] rounded-[10px]'>
+
+      </div>
     </div>
   )
 }
