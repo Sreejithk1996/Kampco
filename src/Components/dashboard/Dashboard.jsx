14604f1b012b2e38df29ff4097ba5cf6fe8b92d5
Sreejithk1996 <sreekumar1996@gmail.com>
2024-03-13 09:18:51 +0530

    updated

--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -13,20 +13,20 @@ function Dashboard() {
         </div>
       </div>
       <div className='flex gap-[20px] '>
-        <div className='w-[100%] h-[200px] bg-[#00A551] rounded-[30px]'>
-          <p>168</p>
+        <div className='w-[100%] h-[200px] bg-[#00A551] rounded-[30px] text-[#FFFF] flex flex-col justify-center items-center'>
+          <p className='text-[36px]'>168</p>
           <p>Services Requested</p>
         </div>
-        <div className='w-[100%] h-[200px] bg-[#F7C35F] rounded-[30px]'>
-          <p>128</p>
+        <div className='w-[100%] h-[200px] bg-[#F7C35F] rounded-[30px] text-[#FFFF] flex flex-col justify-center items-center'>
+          <p className='text-[36px]'>128</p>
           <p>Services In Progress</p>
         </div>
-        <div className='w-[100%] h-[200px] bg-[#E51C26] rounded-[30px]'>
-          <p>40</p>
+        <div className='w-[100%] h-[200px] bg-[#E51C26] rounded-[30px] text-[#FFFF] flex flex-col justify-center items-center'>
+          <p className='text-[36px]'>40</p>
           <p>Services Incomplete</p>
         </div>
-        <div className='w-[100%] h-[200px] bg-[black] rounded-[30px]'>
-          <p>200</p>
+        <div className='w-[100%] h-[200px] bg-[black] rounded-[30px] text-[#FFFF] flex flex-col justify-center items-center'>
+          <p className='text-[36px]'>200</p>
           <p>Total Customers</p>
         </div>
       </div>
